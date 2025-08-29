70bd639fbbd2b23a238a29d48fd0e7d80db67808
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-08-29 15:39:10 +0330

    add icons components

---
+++ src/components/ui/icons/PuzzleIcon.tsx
@@ -0,0 +1,23 @@
+const PuzzleIcon = () => (
+    <svg
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+    >
+        <path d='M14 7V5a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2'></path>
+        <path d='M14 17v2a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2v-2'></path>
+        <path d='M20 7V5a2 2 0 0 0-2-2h-2'></path>
+        <path d='M10 17v2a2 2 0 0 0 2 2h2'></path>
+        <path d='M4 11h16'></path>
+        <path d='M10 3v4'></path>
+        <path d='M10 17v4'></path>
+    </svg>
+);
+
+export default PuzzleIcon;
