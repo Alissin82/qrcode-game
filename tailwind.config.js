c565bdc8fc4ca42eaf3ee60c879ee2c4425151d2
Ali Shahnoushi <alishahnoushi@gmail.com>
2025-08-29 10:55:49 +0330

    config ui theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,36 @@ export default {
     ],
     // Your daisyUI config
     daisyui: {
-        themes: ['light', 'dark'],
+        themes: [
+            {
+                light: {
+                    // This uses the default light theme as a base
+                    ...require('daisyui/src/theming/themes')[
+                        'light'
+                    ],
+
+                    // Now, we override the colors with your custom ones
+                    primary: '#00b48d',
+                    'primary-content': '#ffffff', // Added a contrasting color for text on primary
+                    secondary: '#ef4770',
+                    accent: '#074f9a', // DaisyUI uses 'accent', not 'tertiary'
+                    neutral: '#fcb917', // You can map your colors to semantic names
+
+                    // You can also add your other custom colors here
+                    '--font-display': 'dana',
+                    '--color-primary-light':
+                        '#e6fbfa',
+                    '--color-icon-light':
+                        '#8adcd7',
+                    '--color-primary-text':
+                        '#065b56',
+                },
+            },
+        ],
+        darkTheme: false, // Disables dark mode completely
+        base: true,
+        styled: true,
+        utils: true,
+        logs: true,
     },
 };
